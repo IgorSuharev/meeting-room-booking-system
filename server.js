bb617d1659f96132efe1b54ea0dc8e9901b847c5
Igor Sukharev <new.igorsuharev@gmail.com>
2026-09-06 19:11:43 +0300

    Endpoint /api/rooms with mock-DB created

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
-import { createServer } from "node:http";
-
+import { createServer } from "http";
 const hostname = "127.0.0.1";
 const port = process.env.PORT;
 
-const server = createServer((req, res) => {
+const rooms = [
+  {
+    id: 1,
+    name: "5.12",
+    slotsBooked: [{ login: "new.igorsuharev", time: { start: 12, end: 14 } }],
+  },
+];
+
+const getRoomsHandler = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.write(JSON.stringify(rooms));
+  res.end();
+};
+
+const elseHandler = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 400;
+  res.end("Else case.");
+};
+
+const server = createServer((req, res) => {
+  if (req.url === "/api/rooms" && req.method === "GET") {
+    getRoomsHandler(req, res);
+  } else {
+  }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
