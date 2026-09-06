0d65f7491eddaa1c745304188608cfc4f20341f1
Igor Sukharev <new.igorsuharev@gmail.com>
2026-09-06 15:33:49 +0300

    Added auto-reload on file changes; PORT now in .env

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import { createServer } from 'node:http';
+import { createServer } from "node:http";
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = "127.0.0.1";
+const port = process.env.PORT;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
