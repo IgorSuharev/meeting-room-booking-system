97344f556338e307fa3a701a797fc6bfff53d715
Igor Sukharev <new.igorsuharev@gmail.com>
2026-09-09 07:14:11 +0300

    getRoomByIdHandler refactored

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import { createServer } from "http";
 const hostname = "127.0.0.1";
 const port = process.env.PORT;
 
+// Slot duration is 60 minutes.
 const rooms = [
   {
     id: 1,
     name: "5.12",
-    slotsBooked: [{ login: "new.igorsuharev", time: { start: 12, end: 14 } }],
+    slotsBooked: [{ login: "new.igorsuharev", slot: 14 }],
   },
 ];
 
@@ -20,18 +21,18 @@ const getRoomsHandler = (req, res) => {
 const getRoomByIdHandler = (req, res) => {
   const roomId = parseInt(req.url.split("/rooms/")[1]);
   const room = rooms.find((room) => room.id === roomId);
+  res.setHeader("Content-Type", "application/json");
   if (room) {
-    res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
     res.write(JSON.stringify(room));
-    res.end();
   } else {
-    res.setHeader("Content-Type", "application/json");
     res.statusCode = 404;
     res.write("Room not exist.");
-    res.end();
   }
+  res.end();
 };
+
+const bookRoom = (req, res) => {};
 
 const server = createServer((req, res) => {
   if (req.method === "GET") {
@@ -39,6 +40,10 @@ const server = createServer((req, res) => {
       getRoomsHandler(req, res);
     } else if (req.url.match(/^\/api\/rooms\/(\d+)$/)) {
       getRoomByIdHandler(req, res);
+    }
+  } else if (req.method === "POST") {
+    if (req.url === "/api/book") {
+      bookRoom(req, res);
     }
   }
 });
