c6723e75c070b9b536584f5c8f70ea7045060858
Igor Sukharev <new.igorsuharev@gmail.com>
2026-09-12 13:09:25 +0300

    bookRoom refactored

--- server.js
+++ server.js
@@ -40,50 +40,52 @@ const bookRoom = (req, res) => {
       body += chunk;
     })
     .on("end", () => {
+      let data;
       try {
-        const data = JSON.parse(body);
-        const { id, login, time } = data;
-        if (
-          typeof id !== "number" ||
-          typeof login !== "string" ||
-          typeof time !== "number" ||
-          !Number.isInteger(id) ||
-          !Number.isInteger(time)
-        ) {
-          res.statusCode = 400;
-          res.write(`{"error": "Invalid JSON content."}`);
-          res.end();
-          return;
-        }
-        if (id < 0 || time < 0 || time >= 24) {
-          res.statusCode = 400;
-          res.write(`{"error": "id or time out of range."}`);
-          res.end();
-          return;
-        }
-        const room = rooms.find((room) => room.id === id);
-        if (room) {
-          const slot = room.slotsBooked.find((slot) => slot.time === time);
-          if (!slot) {
-            room.slotsBooked.push({ login, time });
-            res.statusCode = 200;
-            res.write(`{"message": "Slot booked: ${login}, ${time} hrs."}`);
-            res.end();
-          } else {
-            res.statusCode = 404;
-            res.write(`{"error": "Slot already booked by ${slot.login}."}`);
-            res.end();
-          }
-        } else {
-          res.statusCode = 404;
-          res.write(`{"error": "No rooms with id = ${id}."}`);
-          res.end();
-        }
+        data = JSON.parse(body);
       } catch (error) {
         res.statusCode = 400;
         res.write(`{"error": "Invalid JSON."}`);
         res.end();
+        return;
       }
+      const { id, login, time } = data;
+      if (
+        typeof id !== "number" ||
+        typeof login !== "string" ||
+        typeof time !== "number" ||
+        !Number.isInteger(id) ||
+        !Number.isInteger(time)
+      ) {
+        res.statusCode = 400;
+        res.write(`{"error": "Invalid JSON content."}`);
+        res.end();
+        return;
+      }
+      if (id < 0 || time < 0 || time >= 24) {
+        res.statusCode = 400;
+        res.write(`{"error": "id or time out of range."}`);
+        res.end();
+        return;
+      }
+      const room = rooms.find((room) => room.id === id);
+      if (!room) {
+        res.statusCode = 404;
+        res.write(`{"error": "No rooms with id = ${id}."}`);
+        res.end();
+        return;
+      }
+      const slot = room.slotsBooked.find((slot) => slot.time === time);
+      if (slot) {
+        res.statusCode = 409;
+        res.write(`{"error": "Slot already booked by ${slot.login}."}`);
+        res.end();
+        return;
+      }
+      room.slotsBooked.push({ login, time });
+      res.statusCode = 200;
+      res.write(`{"message": "Slot booked: ${login}, ${time} hrs."}`);
+      res.end();
     });
 };
 
